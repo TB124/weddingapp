74e059dac3b932172ea20f276caa1434881e5c3f
Tamas Bondor <t_bondor@yahoo.com>
2018-08-27 20:29:48 +0300

    Controllers
    - Added controllers

---
+++ Websolution/Controllers/PersonController.cs
@@ -0,0 +1,59 @@
+﻿using System.Web.Http;
+using Services.Interface;
+using Services.ServiceModels;
+using Websolution.Controllers.Base;
+
+namespace Websolution.Controllers
+{
+    [AllowAnonymous]
+    [RoutePrefix("api/person")]
+    public class PersonController : BaseApiController
+    {
+        private readonly IPersonService _personService;
+
+        public PersonController(IPersonService personService)
+        {
+            _personService = personService;
+        }
+
+        [HttpGet]
+        [Route("get")]
+        public IHttpActionResult ReadAll()
+        {
+            var persons = _personService.GetAll();
+            return Ok(persons);
+        }
+
+        [HttpGet]
+        [Route("get/{id}")]
+        public IHttpActionResult Read(int id)
+        {
+            var person = _personService.GetById(id);
+            return Ok(person);
+        }
+
+        [HttpPost]
+        [Route("create")]
+        public IHttpActionResult Create(PersonModel model)
+        {
+            var person = _personService.Add(model);
+            return Ok(person);
+        }
+
+        [HttpPut]
+        [Route("update")]
+        public IHttpActionResult Update(PersonModel model)
+        {
+            var person = _personService.Update(model);
+            return Ok(person);
+        }
+
+        [HttpDelete]
+        [Route("delete")]
+        public IHttpActionResult Delete(int id)
+        {
+            _personService.Delete(id);
+            return Ok();
+        }
+    }
+}
---
+++ Websolution/Controllers/TableController.cs
@@ -0,0 +1,59 @@
+﻿using System.Web.Http;
+using Services.Interface;
+using Services.ServiceModels;
+using Websolution.Controllers.Base;
+
+namespace Websolution.Controllers
+{
+    [AllowAnonymous]
+    [RoutePrefix("api/table")]
+    public class TableController : BaseApiController
+    {
+        private readonly ITableService _tableService;
+
+        public TableController(ITableService tableService)
+        {
+            _tableService = tableService;
+        }
+
+        [HttpGet]
+        [Route("get")]
+        public IHttpActionResult ReadAll()
+        {
+            var tables = _tableService.GetAll();
+            return Ok(tables);
+        }
+
+        [HttpGet]
+        [Route("get/{id}")]
+        public IHttpActionResult Read(int id)
+        {
+            var table = _tableService.GetById(id);
+            return Ok(table);
+        }
+
+        [HttpPost]
+        [Route("create")]
+        public IHttpActionResult Create(TableModel model)
+        {
+            var table = _tableService.Add(model);
+            return Ok(table);
+        }
+
+        [HttpPut]
+        [Route("update")]
+        public IHttpActionResult Update(TableModel model)
+        {
+            var table = _tableService.Update(model);
+            return Ok(table);
+        }
+
+        [HttpDelete]
+        [Route("delete")]
+        public IHttpActionResult Delete(int id)
+        {
+            _tableService.Delete(id);
+            return Ok();
+        }
+    }
+}
