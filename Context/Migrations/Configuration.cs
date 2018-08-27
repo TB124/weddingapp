58c9ab421337348c98a3b2994dd4ce381f8fe6ed
Tamas Bondor <t_bondor@yahoo.com>
2018-08-27 19:48:47 +0300

    Repositories and renames
    - Added base Repositories for the new tables
    - renamed some files

--- Context/Migrations/Configuration.cs
+++ Context/Migrations/Configuration.cs
@@ -5,14 +5,14 @@ namespace Context.Migrations
     using System.Data.Entity.Migrations;
     using System.Linq;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<Context.WeddingDbContext>
+    internal sealed class Configuration : DbMigrationsConfiguration<Context.WeddingAppDbContext>
     {
         public Configuration()
         {
             AutomaticMigrationsEnabled = false;
         }
 
-        protected override void Seed(Context.WeddingDbContext context)
+        protected override void Seed(Context.WeddingAppDbContext context)
         {
             //  This method will be called after migrating to the latest version.
 
---
+++ Repositories/Implementation/TableRepository.cs
@@ -0,0 +1,15 @@
+﻿using Ambient.Context.Interfaces;
+using Context.Entities;
+using Infrastructure.Implementation;
+using Repositories.Interface;
+
+namespace Repositories.Implementation
+{
+    public class TableRepository : GenericRepository<Table>, ITableRepository
+    {
+
+        public TableRepository(IAmbientDbContextLocator ambientDbContextLocator) : base(ambientDbContextLocator)
+        {
+        }
+    }
+}
